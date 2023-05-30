57737b25971da49d22ca3b49ae6990f6573d6ac1
Dan Ruan <101991292+Danrley-Ruan-Saquetti@users.noreply.github.com>
2023-05-30 13:17:16 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,64 @@
+export function Drag(args = { itens, zones, itemClassInDragging, itemClassInOverZone, zoneClassOver }) {
+  const itens = document.querySelectorAll(args.itens)
+  const zones = document.querySelectorAll(args.zones)
+
+  const itemClassInDragging = args.itemClassInDragging || "item-dragging"
+  const zoneClassOver = args.zoneClassOver || "item-over-zone"
+  const itemClassInOverZone = args.itemClassInOverZone || "zone-over"
+
+  let itemDragging
+
+  const setup = () => {
+    itens.forEach(item => {
+      item.setAttribute("draggable", "true")
+
+      item.addEventListener('dragstart', dragstart)
+      item.addEventListener('drag', drag)
+      item.addEventListener('dragend', dragend)
+    })
+
+    zones.forEach(zone => {
+      zone.addEventListener('dragenter', dragenter)
+      zone.addEventListener('dragover', dragover)
+      zone.addEventListener('dragleave', dragleave)
+      zone.addEventListener('drop', drop)
+    })
+  }
+
+  // Item
+  function dragstart() {
+    zones.forEach(zone => zone.classList.add(zoneClassOver.split(" ")))
+
+    this.classList.add(itemClassInDragging.split(" "))
+
+    itemDragging = this
+  }
+
+  function drag() { }
+
+  function dragend() {
+    zoneClassOver.split(" ").forEach(className => zones.forEach(zone => zone.classList.remove(className)))
+    itemClassInDragging.split(" ").forEach(className => this.classList.remove(className))
+
+    itemDragging = null
+  }
+
+  // Zone
+  function dragenter() { }
+
+  function dragover() {
+    this.classList.add(itemClassInOverZone.split(" "))
+
+    this.appendChild(itemDragging)
+  }
+
+  function dragleave() {
+    itemClassInOverZone.split(" ").forEach(className => this.classList.remove(className))
+  }
+
+  function drop() {
+    itemClassInOverZone.split(className => this.classList.remove(className))
+  }
+
+  setup()
+}
